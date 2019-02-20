ed20b6fa30b17b5a89cea8f7394aeb7ca71fb874
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-02-20 15:43:00 -0500

    completes stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python
 
+# buying and selling Amazon stock
+
 import argparse
 
-def find_max_profit(prices):
-  pass
+'''
+Write a function `find_max_profit` that receives as input a list of stock prices.
+Your function should return the maximum profit that can be made from a single buy and sell.
+You must buy first before selling; no shorting is allowed here.
 
+HINT: For this problem, we essentially want to find the difference between the smallest and 
+largest prices in the list of prices.
+
+EXAMPLE: Input: [1050, 270, 1540, 3800, 2]) --> Output: 3530
+'''
+def find_max_profit(prices):
+  # set checkvalue at index i,  find max(self - arr[i + 1]) for all i range(0, len - 1)
+
+  # initialize profit to have something to compare against
+  profit = float("-inf")
+
+  # run of for loops is n!/(2 * (n-2)!) = 2 * n * (n - 1) = O(n^2)
+  for i in range(0, len(prices) - 1):
+    for j in range(i + 1, len(prices)):
+      if -(prices[i] - prices[j]) > profit:
+        profit = -(prices[i] - prices[j])
+  
+  return profit
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
