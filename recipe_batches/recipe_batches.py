39b5351149a96935a1d084c9af512f24e2653318
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-02-20 15:57:05 -0500

    completes recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python
 
+'''
+Input:  recipe, available ingredients (both dictionary, key=ingredient name, value=quantity)
+Output: # of whole batches that can be made with available
+'''
+
 import math
 
+# time complexity, O(n) where n = len(recipe)
 def recipe_batches(recipe, ingredients):
-  pass 
+  batches = float('inf')
+
+  try:
+    for key in recipe:
+      if ingredients[key]:
+        batches = min(batches, ingredients[key] // recipe[key])
+    return int(batches)
+  except:
+    return 0
+
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+  ingredients = { 'milk': 200, 'butter': 50, 'flour': 50 }
+  print("{batches} batch(es) can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
