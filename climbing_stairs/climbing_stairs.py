08be66a6e200f44863910a60146c5fee1d53d360
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-02-20 17:09:03 -0500

    completes climbing_stairs.py

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -1,9 +1,67 @@
 #!/usr/bin/python
 
+'''
+REQUIREMENT:
+- counts the number of possible ways in which the child can ascend the staircase (1, 2, or 3 steps)
+
+HINTS:
+- a bunch of possible permutations, use recursion
+- base cases (climb 0 stairs? negative number of stairs?)
+- how recursively call such that we move towards one or more of these base cases?
+- caching/memoization might be one avenue
+
+EXAMPLES:
+climbing_stairs(0) --> 1
+climbing_stairs(1) --> 1
+climbing_stairs(2) --> 2
+climbing_stairs(3) --> 4 = climb(0) + climb(1) + climb(2)
+climbing_stairs(4) --> 7 = climb(1) + climb(2) + climb(3)
+climbing_stairs(5) --> 13
+climbing_stairs(10) --> 274
+
+NOTES:
+Number of solutions to x + 2y + 3x = n where all are positive integers (including 0)
+Sum of previous 3 climbs
+'''
+
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
+  # invalid stairs
+  if n < 0:
+    return 0
+
+  # base cases
+  if n == 0:
+    if cache:
+      cache[n] = 1
+    return 1
+  if n == 1:
+    if cache:
+      cache[n] = 1
+    return 1
+  if n == 2:
+    if cache:
+      cache[n] = 2
+    return 2
+
+  # return if received cache argument
+  if cache:
+    if cache[n]:
+      return cache[n]
+  
+    val = climbing_stairs(n - 3, cache) + climbing_stairs(n - 2, cache) + climbing_stairs(n - 1, cache)
+    cache[n] = val
+
+    return val
+  # return if no cache argument
+  else:
+    return climbing_stairs(n - 3) + climbing_stairs(n - 2) + climbing_stairs(n - 1)
+
+
+  
+  
+
 
 
 if __name__ == "__main__":
